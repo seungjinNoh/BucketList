4a03ce105fd89a881b1ca69a04ef1f68898d43bd
seungjin <seungjin3047@gmail.com>
2023-03-01 08:47:36 +0900

    feat: database insert 추가
    database WorkDao insert 추가

--- data/src/main/java/com/example/data/local/dao/WorkDao.kt
+++ data/src/main/java/com/example/data/local/dao/WorkDao.kt
@@ -1,14 +1,18 @@
 package com.example.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.data.local.model.Work
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface WorkDao {
-
     @Query("SELECT * FROM ${Work.TABLE_NAME}")
     fun getAllWork(): Flow<List<Work>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insert(work: Work)
 
 }
