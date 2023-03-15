6905aa92f1aa385f9f81efde2640da3e1e48e470
Seungjin1102 <seungjin3047@gmail.com>
2023-03-15 17:48:40 +0900

    feat: UpdateFragment DB insert 연산 연결
    insert 연산 연결 추후 UI 수정 필요

--- app/src/main/java/com/example/bucketlist/view/UpdateFragment.kt
+++ app/src/main/java/com/example/bucketlist/view/UpdateFragment.kt
@@ -4,33 +4,54 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.MutableLiveData
 import com.example.bucketlist.R
+import com.example.bucketlist.application.BucketApplication
 import com.example.bucketlist.base.BaseFragment
-import com.example.bucketlist.databinding.FragmentMainBinding
 import com.example.bucketlist.databinding.FragmentUpdateBinding
+import com.example.bucketlist.utils.ResourceUtils
 import com.example.bucketlist.viewmodel.WorkViewModel
 import com.example.domain.model.WorkEntity
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
 
 @AndroidEntryPoint
 class UpdateFragment : BaseFragment<FragmentUpdateBinding, WorkViewModel>(R.layout.fragment_update) {
 
     override val viewModel: WorkViewModel by viewModels()
 
+    val liveTitle = MutableLiveData<String>()
+    val liveContents = MutableLiveData<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        val view = super.onCreateView(inflater, container, savedInstanceState)
+        binding.fragment = this@UpdateFragment
+
+        return view
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    fun checkWorkData() {
+        if (liveTitle.value.isNullOrBlank()) {
+            Toast.makeText(BucketApplication.applicationContext(), ResourceUtils.getString(R.string.update_title) , Toast.LENGTH_SHORT).show()
+            return
+        }
+        if (liveContents.value.isNullOrBlank()) {
+            Toast.makeText(BucketApplication.applicationContext(), ResourceUtils.getString(R.string.update_contents) , Toast.LENGTH_SHORT).show()
+            return
+        }
 
+        viewModel.insertWork(
+            WorkEntity(
+                liveTitle.value!!,
+                liveContents.value!!
+            )
+        )
 
     }
+
 }
